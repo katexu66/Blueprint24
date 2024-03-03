3e39fc4da3e667c3999f18040b1a2e4efeb34b22
octo noodle <cat.burrito@icloud.com>
2024-03-03 11:42:24 -0500

    add distance sensor loop (code on conflicting pins commented out)

--- blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
+++ blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
@@ -1,22 +1,22 @@
 // wiring diagram 
 /*
 
-         \/ the arduino \/
-        __________________
-       [------------------]13 
-       [------------------]12 distance sensor TRIG
-    RST[------------------]11 distance sensor ECHO
-    3.3[------------------]10
-     5V[------------------]9 buzzer (1)
-    GND[------------------]8
-    GND[------------------]7
-    Vin[------------------]6
-     A0[------------------]5
-     A1[------------------]4
-     potentiometer buzzer A2[------------------]3
-     A3[------------------]2 button for potentiometer
-     A4[------------------]1
-     potentiometer A5[------------------]0
+                         \/ the arduino \/
+                        __________________
+                       [------------------]13 
+                       [------------------]12 distance sensor TRIG
+                    RST[------------------]11 distance sensor ECHO
+                    3.3[------------------]10
+                     5V[------------------]9 buzzer (1)
+                    GND[------------------]8
+                    GND[------------------]7
+                    Vin[------------------]6
+                     A0[------------------]5
+                     A1[------------------]4
+potentiometer buzzer A2[------------------]3
+                     A3[------------------]2 button for potentiometer
+                     A4[------------------]1
+       potentiometer A5[------------------]0
 
 
 
@@ -33,7 +33,7 @@
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 48 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-#define LED 11
+//#define LED 11
 #define BUTTON 2
 #define POT_PIN A5
 #define JOYSTICK_X A4
@@ -46,10 +46,15 @@
 TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);  // pin D9
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
+// options
+#define bpm 6000
+int sampleRate = 60000 / bpm;
+
 void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud
 
   // pin modes
-  pinMode(LED, OUTPUT);
+//  pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(POT_PIN, INPUT);
   pinMode(JOYSTICK_X, INPUT);
@@ -57,16 +62,18 @@ void setup() {
   pinMode(JOYSTICK_BTN, INPUT_PULLUP);
   pinMode(ANA_BUZZER, OUTPUT);
   pinMode(DIGI_BUZZER, OUTPUT);
+  pinMode(BUZZER_PWM_PIN, OUTPUT);
+  analogWrite(BUZZER_PWM_PIN, 512);
 
 }
 
 // LED
-void lightOn() {
-  digitalWrite(LED, HIGH);
-}
-void lightOff() {
-  digitalWrite(LED, LOW);
-}
+// void lightOn() {
+//   digitalWrite(LED, HIGH);
+// }
+// void lightOff() {
+//   digitalWrite(LED, LOW);
+// }
 
 // buzzers
 void digiBuzzerOn() {
@@ -76,7 +83,25 @@ void digiBuzzerOff() {
   digitalWrite(DIGI_BUZZER, LOW);
 }
 void anaBuzzerOff() {
-  noTone(ANA_BUZZER);
+  //noTone(ANA_BUZZER);
+}
+
+void setDistanceBuzzer(int val, int min, int max) {
+  if (val == 0) {
+    return;
+  }
+  int scaled = getTone(map(val, min, max, 0, 12));
+  Serial.println(scaled);
+  tone1.tone(scaled);
+}
+
+void distSensorUpdate() {
+  delay(sampleRate);  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  int distance = sonar.ping_cm();
+  Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
+  setDistanceBuzzer(distance, 0, MAX_DISTANCE);
 }
 
 void button() {
@@ -97,7 +122,7 @@ void button() {
 void pot() {
   int pot_value = analogRead(POT_PIN);
   int ana_value = map(pot_value, 0, 1023, 262, 523);
-  tone(ANA_BUZZER, ana_value);
+  //tone(ANA_BUZZER, ana_value);
 
   // if (pot_value > 512) {
   //   lightOn();
@@ -106,8 +131,12 @@ void pot() {
   // }
 }
 
+// processing 
+int getTone(int value) {
+  return 440 * pow(1.0594631, value);
+}
+
 void loop() {
-
+  distSensorUpdate();
   button();
-
 }
