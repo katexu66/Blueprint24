b180668a75c0ad7662ef1e76e5e7c024e7e49b43
octo noodle <cat.burrito@icloud.com>
2024-03-03 11:45:33 -0500

    removed testing buzzer/led stuff

--- blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
+++ blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
@@ -33,14 +33,12 @@ potentiometer buzzer A2[------------------]3
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 48 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-//#define LED 11
 #define BUTTON 2
 #define POT_PIN A5
 #define JOYSTICK_X A4
 #define JOYSTICK_Y A3
 #define JOYSTICK_BTN 3
 #define ANA_BUZZER A2
-#define DIGI_BUZZER 4
 
 // library objects
 TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);  // pin D9
@@ -54,34 +52,18 @@ void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud
 
   // pin modes
-//  pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(POT_PIN, INPUT);
   pinMode(JOYSTICK_X, INPUT);
   pinMode(JOYSTICK_Y, INPUT);
   pinMode(JOYSTICK_BTN, INPUT_PULLUP);
   pinMode(ANA_BUZZER, OUTPUT);
-  pinMode(DIGI_BUZZER, OUTPUT);
   pinMode(BUZZER_PWM_PIN, OUTPUT);
   analogWrite(BUZZER_PWM_PIN, 512);
 
 }
 
-// LED
-// void lightOn() {
-//   digitalWrite(LED, HIGH);
-// }
-// void lightOff() {
-//   digitalWrite(LED, LOW);
-// }
-
 // buzzers
-void digiBuzzerOn() {
-  digitalWrite(DIGI_BUZZER, HIGH);
-}
-void digiBuzzerOff() {
-  digitalWrite(DIGI_BUZZER, LOW);
-}
 void anaBuzzerOff() {
   //noTone(ANA_BUZZER);
 }
@@ -104,31 +86,19 @@ void distSensorUpdate() {
   setDistanceBuzzer(distance, 0, MAX_DISTANCE);
 }
 
+// control
 void button() {
-
   if (digitalRead(BUTTON) == 0) {
     pot();
   } else {
     anaBuzzerOff();
   }
-
-  // if (digitalRead(BUTTON) == 0) {
-  //   digiBuzzerOn();
-  // } else {
-  //   digiBuzzerOff();
-  // }
 }
 
 void pot() {
   int pot_value = analogRead(POT_PIN);
   int ana_value = map(pot_value, 0, 1023, 262, 523);
   //tone(ANA_BUZZER, ana_value);
-
-  // if (pot_value > 512) {
-  //   lightOn();
-  // } else {
-  //   lightOff();
-  // }
 }
 
 // processing 
