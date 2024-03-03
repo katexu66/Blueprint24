7bcb2b43eac67fc1e7227a9a93d89ca32eba6aff
octo noodle <cat.burrito@icloud.com>
2024-03-03 10:57:39 -0500

    library to fix distance sensor/tone library hardware conflict (install in arduino ide)

--- one-dimension-test/one-dimension-test.ino
+++ one-dimension-test/one-dimension-test.ino
@@ -1,18 +1,35 @@
 #include <NewPing.h>
+#include 
 
-#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define BUZZER_PWM_PIN 9
+
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     7  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 50 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  pinMode(BUZZER_PWM_PIN, OUTPUT);
+  analogWrite(BUZZER_PWM_PIN, 512);
+}
+
+void setBuzzer(int val, int min, int max) {
+  int scaled = getTone(map(val, min, max, 0, 12));
+  Serial.println(scaled);
+  //tone(BUZZER_PWM_PIN, scaled);
+}
+
+int getTone(int value) {
+  return 440 * pow(1.0594631, value);
 }
 
 void loop() {
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  delay(5);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+  int distance = sonar.ping_cm();
+  Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("carcm");
+  setBuzzer(distance, 0, MAX_DISTANCE);
 }
