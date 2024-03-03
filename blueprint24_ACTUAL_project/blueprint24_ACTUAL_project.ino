8a0a3e5a95bc160a87edb5fa4a9d530ac7331a25
octo noodle <cat.burrito@icloud.com>
2024-03-03 13:22:54 -0500

    added scale modes, including hard-coded two-octave c major scale

--- blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
+++ blueprint24_ACTUAL_project/blueprint24_ACTUAL_project.ino
@@ -32,22 +32,23 @@ potentiometer buzzer A2[------------------]3 metronnome buzzer
 #define BUZZER_PWM_PIN 9
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 48 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define BUTTON 2
 #define POT_PIN A3
 #define JOYSTICK_X A4
+#define JOYSTICK_Y A1
 #define JOYSTICK_BTN 3
 #define ANA_BUZZER A2
-
-// library objects
-TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);  // pin D9
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 // options
 #define bpm 2000 // max: 2000 for "smooth" sampling
 int sampleRate = 60000 / bpm;
 #define INERTIA_MODE true
 #define INERTIA_AMOUNT 10 // number of previous values remembered
+#define MAX_DISTANCE 96 // Maximum distance to ping at (cm). Maximum sensor distance is rated at 400-500cm.
+
+// library objects
+TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);  // pin D9
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud
@@ -65,8 +66,58 @@ void setup() {
 }
 
 // processing 
-int getTone(int value) {
-  return 440 * pow(1.0594631, value);
+
+/*
+0 - "free": half steps
+1 - "maj/major": c major scale
+2 - "min/minor": a minor scale
+3 - "pen/pentatonic" pentatonic scale
+4 - "arp/arpeggio" c arpeggio
+*/
+int getTone(int value, int option) {
+  switch (option) {
+    case 0:
+      return 440 * pow(1.0594631, value);
+    case 1:
+      switch (value) {
+        case 0:
+          return 523; // C5
+        case 1:
+          return 587; // D5
+        case 2:
+          return 659; // E5
+        case 3:
+          return 698; // F5
+        case 4:
+          return 784; // G5
+        case 5:
+          return 880; // A5
+        case 6:
+          return 988; // B5
+        case 7:
+          return 1047; // C6
+        case 8:
+          return 1175; // D6
+        case 9:
+          return 1319; // E6
+        case 10:
+          return 1397; // F6
+        case 11:
+          return 1568; // G6
+        case 12:
+          return 1760; // A6
+        case 13:
+          return 1976; // B6
+        case 14:
+          return 2093; // C7
+        default:
+          return 262; // C4
+        
+      }
+    default:
+     return 440;
+  }
+  
 }
 
 int prevDists[INERTIA_AMOUNT]; // store last twenty samplings to average out and make changes smoother
@@ -85,11 +136,11 @@ int nextSample(int newValue) {
 
 // buzzers
 void anaBuzzerOff() {
-  noTone(ANA_BUZZER);
+  //noTone(ANA_BUZZER);
 }
 
 void setDistanceBuzzer(int val, int min, int max) {
-  int scaled = getTone(map(val, min, max, 0, 12));
+  int scaled = getTone(map(val, min, max, 0, 12), 1);
   Serial.println(scaled);
   tone1.tone(scaled);
 }
@@ -128,7 +179,7 @@ void button() {
 void pot() {
   int pot_value = analogRead(POT_PIN);
   int ana_value = map(pot_value, 0, 1023, 262, 523);
-  tone(ANA_BUZZER, ana_value);
+  //tone(ANA_BUZZER, ana_value);
 }
 
 void loop() {
